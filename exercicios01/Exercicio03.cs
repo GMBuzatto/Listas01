a0deb8cbac7ec617da47c9d265d810018d6888f2
GMBuzatto <gabrielmartinbuzatto1999@gmail.com>
2021-12-11 10:21:52 -0300

    Exercicio03

---
+++ exercicios01/Exercicio03.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicios01
+{
+    public class Exercicio03
+    {
+        public double ConsumoMedio(double distanciaTotal,double totalCombustivelGasto)
+        {
+            return distanciaTotal / totalCombustivelGasto;
+        }
+    }
+}
--- exercicios01/Program.cs
+++ exercicios01/Program.cs
@@ -11,7 +11,8 @@ namespace exercicios01
             Console.WriteLine("Entre com o segundo valor: ");
             double valor2 = Convert.ToDouble(Console.ReadLine());
             Exercicio01 exercicio01 = new Exercicio01();
-            Console.WriteLine("Exercicio 01 :" + exercicio01.Soma(valor1, valor2));
+            Console.WriteLine("Exercicio 01: " + exercicio01.Soma(valor1, valor2));
+            Console.WriteLine("___________________________________________________________________");
             Exercicio02 exercicio02 = new Exercicio02();
             Console.WriteLine("Entre com o primeiro valor: ");
             valor1 = Convert.ToDouble(Console.ReadLine());
@@ -19,7 +20,15 @@ namespace exercicios01
             valor2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Entre com o nome da operação:");
             string operacao = Console.ReadLine();
-            Console.WriteLine("Exercicio 02 :"+ exercicio02.Operacao(operacao,valor1,valor2));
+            Console.WriteLine("Exercicio 02: "+ exercicio02.Operacao(operacao,valor1,valor2));
+            Console.WriteLine("___________________________________________________________________");
+            Exercicio03 exercicio03 = new Exercicio03();
+            Console.WriteLine("Distância total percorrida pelo automóvel: ");
+            double distanciaTotal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Total de combustível gasto: ");
+            double totalCombustivelGasto = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Exercicio 03: "+ exercicio03.ConsumoMedio(distanciaTotal,totalCombustivelGasto) +"KM/L");
+            Console.WriteLine("___________________________________________________________________");
         }
     }
 }
